ee7f57f4c1fe0fcb14b03328403a51ab11ac1f43
jhraga <jhraga@gmail.com>
2018-05-23 07:24:41 -0400

    Generacion de todos los compoentes y su ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { RetroComponent } from './retro/retro.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'team', component: TeamComponent },
+  { path: 'retro', component: RetroComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamComponent,
+    RetroComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
